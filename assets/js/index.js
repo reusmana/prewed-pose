31c15af3ba3513fac7bf028b81c25b7163b24b3f
reusmana <reusmanasujani@gmail.com>
2024-12-10 14:52:21 +0700

    first:commit

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+const imagesJson = await fetch("/assets/images/index.json").then((res) =>
+  res.json()
+);
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("res");
+// });
+
+const poseContainer = document.querySelector("#pose");
+
+imagesJson.forEach((image) => {
+  const container = document.createElement("div");
+  container.className =
+    "relative h-fit group cursor-pointer md:w-1/2 xl:w-1/3 rounded-2xl overflow-hidden";
+
+  // Set the inner HTML for the div
+  container.innerHTML = `
+      <img src="./assets/images/${image.images}" alt="" />
+      <div
+        class="absolute group-hover:flex hidden transition-all ease-in-out duration-500 inset-0 bg-black bg-opacity-45 text-6xl text-white font-bold justify-center items-center"
+      >
+        ${image.id}
+      </div>
+    `;
+
+  // Append the container to the pose container
+  poseContainer.appendChild(container);
+});
